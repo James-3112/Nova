b506df06eeb7c6e9e1db6c23befce154af21811b
PikachuGamer31 <jamesturner07@icloud.com>
2025-12-17 19:39:54 +1000

    Changed layer stack to use a c# stack

--- Nova/Application.cs
+++ Nova/Application.cs
@@ -2,14 +2,14 @@ namespace Nova;
 
 
 public class Application {
-    public LayerStack layerStack;
+    public Stack<Layer> layerStack;
     private Window window;
 
     public Application() {
         Input.SetBackend(Input.Backend.Raylib);
 
-        layerStack = new LayerStack();
-        layerStack.AddLayer(new RenderLayer(RenderLayer.Backend.Raylib));
+        layerStack = new Stack<Layer>();
+        layerStack.Push(new RenderLayer(RenderLayer.Backend.Raylib));
 
         window = Window.CreateWindow(Window.Backend.Raylib);
     }
@@ -18,7 +18,9 @@ public class Application {
         window.Open();
 
         while (window.WindowShouldClose() == false) {
-            layerStack.UpdateLayers();
+            foreach (Layer layer in layerStack) {
+                layer.OnUpdate();
+            }
         }
 
         OnShutdown();
